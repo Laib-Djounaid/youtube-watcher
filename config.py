be313c71a1c441f338e9e362350225b9103be37f
Mohamed Djounaid LAIB <96971956+Laib-Djounaid@users.noreply.github.com>
2024-06-05 08:32:09 +0100

    yoo

--- config.py
+++ config.py
@@ -1,4 +1,16 @@
 config= {
     'google_api_key' : 'AIzaSyA2EuEavNC2qKY1_uE2eZFW74z4OsxsGso' ,
-    'youtube_playlist_id' : 'PLUQORQEatnJeo-BiGjRopl5iK5CStc38t'
+    'youtube_playlist_id' : 'PLUQORQEatnJeo-BiGjRopl5iK5CStc38t' ,
+    'kafka' : {
+        "bootstrap.servers" : "pkc-l6wr6.europe-west2.gcp.confluent.cloud:9092" ,
+        #security and authentification stuff
+        "security.protocol" : "sasl_ssl" ,
+        "sasl.mechanism" : "PLAIN" ,
+        "sasl.username" : "MIFFA2LXGW7HCOEI" ,
+        "sasl.password" : "dhyyEp59gne0O83+1KWRu0surlWVHKsSvTyEd3hWl4POY7Q9Ljp6AOCza6oeOTQZ" ,
+    } ,
+    'schema_registry' : {
+        "url" : "https://psrc-gqrvzv.southeastasia.azure.confluent.cloud" ,
+        "basic.auth.user.info" : "WDL3XLBW5IEVVIOI:L3RcwmQlquKjBwIZdQBdTM6+mwdvBByxS3q1pWcRkvpJLMLWTUcuHYnGbEfRpYTt",
+    }
 }
